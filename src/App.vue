dfd2c1f0db3ddcca89bb57d3c9eefc8ce4a46391
SashaKa <katushkovasa@gmail.com>
2017-12-08 18:23:28 +0300

    initial

---
+++ src/App.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="container">
+    <app-header :quoteCount="quotes.length" :maxQuotes="maxQuotes"></app-header>
+    <app-new-quote @quoteAdded="newQuote"></app-new-quote>
+    <app-quotes-grid :quotes="quotes" @quoteDeleted="deleteQuote"></app-quotes-grid>
+    <div class="row">
+      <div class="col-sm-12" text-center>
+        <div class="alert alert-info">Click on a quote to delete it </div>
+      </div>
+      <div class="col-sm-12" text-center>
+        <div class="alert alert-info">
+         <div :alertQuotes="alertQuotes"> </div> 
+        </div>
+      </div>       
+    </div>
+  </div>
+</template>
+<script>
+  import QuotesGrid from './components/QuotesGrid'
+  import NewQuote from './components/NewQuote'
+  import Header from './components/Header'
+  export default {
+    data: function () {
+      return {
+        quotes: [
+          'Type your own quote'
+        ],
+        maxQuotes: 10,
+        alertQuotes: ''
+      }
+    },
+    methods: {
+      newQuote(quote) {
+        if(this.quotes.length>=this.maxQuotes){
+          
+           return console.log('Pleace, delete quote at first!')
+          
+        }
+        this.quotes.push(quote)
+      },
+      deleteQuote(i) {
+        this.quotes.splice(i, 1)
+      }
+    },
+    components: {
+      appQuotesGrid: QuotesGrid,
+      appNewQuote: NewQuote,
+      appHeader: Header
+    }
+  }
+</script>
+<style>
+</style>
---
+++ src/components/Quote.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="col-sm-6 col-md-6 col-lg-3">
+    <div class="panel panel-default">
+      <div class="panel-body quote">
+        <slot></slot>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+  export default {
+    props: ['quotes']
+  }
+</script>
+<style>
+  .panel-body {
+    font-family: 'Barlow Condensed', sans-serif;
+    font-size: 24px;
+    color: rgb(58, 9, 104);
+  }
+
+  .quote {
+    cursor: pointer;
+  }
+
+  .quote:hover {
+    color: rgb(218, 203, 232)
+  }
+</style>
